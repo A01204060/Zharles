0fcb4c1b546a8074082ed3b589070e9b4fb88108
zharles <160550301+Zharless@users.noreply.github.com>
2024-03-13 20:06:59 -0600

    Memory Grid

--- pre-prototype.py
+++ pre-prototype.py
@@ -1,12 +1,15 @@
-#attempt to make a grid
-
-def my_print_square(d):
-    all_ = d * d
-    x = list(range(all_))
-    x.sort(reverse=True)  
-    i=0
-    while i < all_:
-       print(" ".join(map(str, x[i:i+d])))
-       i += d
+# principal funcion for the contruction of the Grid using for loops and one conditional with the staetment __name__ == '__main__'
+def print_grid(grid):
+    
+    for row in grid:
+        for column in row:
+            print(column, end=' ')
+        print()
 
 
+if __name__ == '__main__':
+    # list of numbers for the values inside the grid 
+    numbers = [[1, 2, 3, 1],[4, 5, 6, 2],[3, 6, 5, 4]]
+
+
+print_grid(numbers)
